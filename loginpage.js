e947701d1e67379af0a949d38e6c3b2f2b12d73e
Bernice Chiaha <bernicechiaha@dynamic-10-125-160-140.dynamic.unt.edu>
2024-03-19 12:17:36 -0500

    Fixed the Navigation pane

--- loginpage.js
+++ loginpage.js
@@ -1,8 +1,38 @@
 
+/*
 document.getElementById("hamburger").addEventListener("click", function() {
   document.getElementById("nav").classList.toggle("open");
 });
 
 function closeNav() {
   document.getElementById("nav").classList.remove("open");
+}
+*/
+function toggleNav() {
+  var sidenav = document.getElementById("mySidenav");
+  if (sidenav.style.width == "200px") {
+    closeNav();
+  } else {
+    openNav();
+  }
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "200px";
+  document.getElementById("main").style.marginRight = "200px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+}
+
+
+function bcrypt_encrypt(username, password) {
+	bcrypt.genSalt(10, function(err, salt) {
+		bcrypt.hash(password, salt, function(err, key) {
+			console.log("bcrypt Store the salt: " + salt + " and hash: " + key)
+			bcrypt_decrypt(password, salt, key)
+		});
+	});
 }
