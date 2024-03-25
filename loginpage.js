63ae112901d59e6166b82cd4604541d119f9aad3
Bernice <bernicechiaha@my.unt.edu>
2024-03-25 15:04:35 -0500

    Fixed the Navigation pane

--- loginpage.js
+++ loginpage.js
@@ -8,7 +8,7 @@ function closeNav() {
   document.getElementById("nav").classList.remove("open");
 }
 */
-function toggleNav() {
+function toggleNavigation() {
   var sidenav = document.getElementById("mySidenav");
   if (sidenav.style.width == "200px") {
     closeNav();
@@ -20,11 +20,13 @@ function toggleNav() {
 function openNav() {
   document.getElementById("mySidenav").style.width = "200px";
   document.getElementById("main").style.marginRight = "200px";
+  document.querySelector(".hamburger").classList.add("open");  
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginRight= "0";
+  document.querySelector(".hamburger").classList.remove("open"); 
 }
 
 /*
