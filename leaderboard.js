7c7dfb3b78ff491e0a6f27e323d8277b0a62ce4f
antonettepenn <77859337+antonettepenn@users.noreply.github.com>
2024-03-26 16:17:09 +0000

    made leaderboard smaller and centered

--- leaderboard.js
+++ leaderboard.js
@@ -1,29 +1,50 @@
 // Sample leaderboard data
 const leaderboardData = [
-  { rank: 1, name: 'PLACE HOLDER'},
-  { rank: 2, name: 'PLACE HOLDER'},
-  { rank: 3, name: 'PLACE HOLDER'},
-  { rank: 4, name: 'PLACE HOLDER' },
-  { rank: 5, name: 'PLACE HOLDER'}
-];
+    { rank: 1, UserName: 'PLACE HOLDER'},
+    { rank: 2, UserName: 'PLACE HOLDER'},
+    { rank: 3, UserName: 'PLACE HOLDER'},
+    { rank: 4, UserName: 'PLACE HOLDER' },
+    { rank: 5, UserName: 'PLACE HOLDER'},
+    { rank: 6, UserName: 'PLACE HOLDER'},
+    { rank: 7, UserName: 'PLACE HOLDER'},
+    { rank: 8, UserName: 'PLACE HOLDER'},
+    { rank: 9, UserName: 'PLACE HOLDER' },
+    { rank: 10, UserName: 'PLACE HOLDER'}
+  ];
+  
+  // Encapsulate Variable
+  class Leaderboard {
+        constructor(data) {
+        this._data = data;
+        }
+    
+        get data() {
+        return this._data;
+        }
+    
+        set data(data) {
+        this._data = data;
+        }
 
-
-function makeBoard() {
-  const leaderboardTable = document.getElementById('leaderboard');
-  const leaderboardBody = leaderboardTable.querySelector('tbody');
-
-
-  leaderboardBody.innerHTML = '';
-
-
-  leaderboardData.forEach(player => {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-      <td>${player.rank}</td>
-      <td>${player.name}</td>
-    `;
-    leaderboardBody.appendChild(row);
-  });
-}
-
-makeBoard();
+    // Extract Method
+        makeBoard() {
+            const leaderboardTable = document.getElementById('leaderboard');
+            const leaderboardBody = leaderboardTable.querySelector('tbody');
+        
+            leaderboardBody.innerHTML = '';
+        
+            this.data.forEach(user => {
+              const row = document.createElement('tr');
+              row.innerHTML = `
+                <td>${user.UserName}</td>
+                <td>${user.rank}</td>
+              `;
+              leaderboardBody.appendChild(row);
+            });
+          }
+    }
+        
+    
+  const leaderboard = new Leaderboard(leaderboardData);
+  leaderboard.makeBoard();
+  
