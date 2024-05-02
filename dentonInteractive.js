e1f3dc23f228c11354c097f4c6a7f02904b3938b
Olose, Prisca <priscaolose@my.unt.edu>
2024-05-02 12:42:08 -0500

    updated pages

--- dentonInteractive.js
+++ dentonInteractive.js
@@ -1,6 +1,6 @@
 const apiKey = 'pk.eyJ1IjoicHJpc2Nhb2xvc2UiLCJhIjoiY2x2MTNsN2FrMDBwODJqbjVkaHBmbDFpOCJ9.L_CJ3_ewqWvkIiQ__-mGCQ'
 
-const map = L.map('map').setView([33.2148, -97.133064], 13);
+const map = L.map('map').setView([33.2148, -97.133064], 12);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     maxZoom: 19,
@@ -13,18 +13,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 //creating park icon for marker
 const parkIcon = L.icon({
     iconUrl: 'pin.png',
-
-    iconSize:     [39, 50], // size of the icon
+    iconSize:     [30, 50], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
-//const parkIcon2 = L.icon({
-    //iconUrl: 'images/nature.png',
-
-    //iconSize:     [40, 50], // size of the icon
-    //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-//});
 
 //Adding marker
 
@@ -35,50 +27,91 @@ const MckennaParkmarker = L.marker([33.2202, -97.1599], {icon: parkIcon}).addTo(
 const WheelerRidgePmarker = L.marker([33.1547, -97.0975], {icon: parkIcon}).addTo(map);
 //const NLPmarker = L.marker([33.240883, -97.153856], {icon: parkIcon}).addTo(map);
 
-//Adding pop up message to marker
+//Ad// Park 1 - Eureka Park
 let park1 = `
 <div style="text-align:center">
     <a href="eureka.html">
-        <img width="150" height="150" src="/images/Eureka_Park.jpeg"/>
+        <img src="images/Eureka_Park.jpeg" alt="Eureka Park" width="100%" height="100">
     </a>
     <h4>Eureka Park</h4>
+    <p>Location: <a href="https://www.google.com/maps/dir/?api=1&destination=506+Hobson+Ln%2C+Denton%2C+TX+76205" target="_blank">506 Hobson Ln, Denton, TX 76205</a></p>
+    <p>Accessibility features:<br>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Parking: Yes</p>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Ramps: Yes</p>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Bathrooms: Yes</p>
+    </p>
+    <a href="eureka.html">Learn more</a>
 </div>
 `
 
+// Park 2 - North Lakes Park
 let park2 = `
 <div style="text-align:center">
     <a href="nlp.html">
-        <img width="150" height="150" src="/images/North_Lakes_Park.jpeg"/>
+        <img width="100%" height="100" src="images/North_Lakes_Park.jpeg"/>
     </a>
     <h4>North Lakes Park</h4>
+    <p>Location: <a href="https://www.google.com/maps/dir/?api=1&destination=Addison+Ave%2C+Denton%2C+TX+76201" target="_blank">Addison Ave, Denton, TX 76201</a></p>
+    <p>Accessibility features:<br>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Parking: Yes</p>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Ramps: Yes</p>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Bathrooms: Yes</p>
+    </p>
+    <a href="nlp.html">Learn more</a>
 </div>
 `
 
+// Park 3 - Lake Forest Park
 let park3 = `
 <div style="text-align:center">
     <a href="lakeForest.html">
-        <img width="150" height="150" src="/images/lakeForest_park.jpeg"/>
+        <img width="100%" height="100" src="images/lakeForest_park.jpeg"/>
     </a>
     <h4>Lake Forest Park</h4>
-</div>
-`
-let park4 = `
-<div style="text-align:center">
-    <a href="mckenna.html">
-        <img width="150" height="150" src="/images/mckennaPark.jpeg"/>
-    </a>
-    <h4>Mckenna Park</h4>
+    <p>Location: <a href="https://www.google.com/maps/dir/?api=1&destination=4150+Keaton+Dr%2C+Denton%2C+TX+76210" target="_blank">4150 Keaton Dr, Denton, TX 76210</a></p>
+    <p>Accessibility features:<br>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Parking: Yes</p>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Ramps: Yes</p>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Bathrooms: Yes</p>
+    </p>
+    <a href="lakeForest.html">Learn more</a>
 </div>
 `
 
+// Park 4 - Mckenna Park
+let park4 = `
+<div style="text-align:center">
+    <a href="mckenna.html">
+        <img width="100%" height="100" src="images/mckennaPark.jpeg"/>
+    </a>
+    <h4>Mckenna Park</h4>
+    <p>Location: <a href="https://www.google.com/maps/dir/?api=1&destination=2001+Old+Alton+Rd%2C+Denton%2C+TX+76210" target="_blank">2001 Old Alton Rd, Denton, TX 76210</a></p>
+    <p>Accessibility features:<br>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Parking: Yes</p>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Ramps: Yes</p>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Bathrooms: Yes</p>
+    </p>
+    <a href="mckenna.html">Learn more</a>
+</div>
+`
+
+// Park 5 - Wheeler Ridge Park
 let park5 = `
 <div style="text-align:center">
     <a href="wheeler.html">
-        <img width="150" height="150" src="images/Wheeler-ridge-park.jpeg"/>
+        <img width="100%" height="100" src="images/Wheeler-ridge-park.jpeg"/>
     </a>
     <h4>Wheeler Ridge Park</h4>
+    <p>Location: <a href="https://www.google.com/maps/dir/?api=1&destination=1022+E.+Wheeler+Ridge+Ave%2C+Denton%2C+TX+76205" target="_blank">1022 E. Wheeler Ridge Ave, Denton, TX 76205</a></p>
+    <p>Accessibility features:<br>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Parking: Yes</p>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Ramps: Yes</p>
+    <p style="background-color: white; border-radius: 50px; margin = 100px; font-weight:bold;">Bathrooms: Yes</p>
+    </p>
+    <a href="wheeler.html">Learn more</a>
 </div>
 `
+
 
 EurekaPmarker.bindPopup(park1);
 NorthLakesPmarker.bindPopup(park2);
@@ -86,46 +119,45 @@ LakeForestPmarker.bindPopup(park3);
 MckennaParkmarker.bindPopup(park4);
 WheelerRidgePmarker.bindPopup(park5);
 
-document.addEventListener("DOMContentLoaded", function() {
-    var checkbox = document.getElementById("toggleCheckbox");
-    var pageIndicator = document.getElementById("pageIndicator");
 
-     // Function to update page indicator text based on checkbox state
-     function updatePageIndicator() {
-        if (checkbox.checked) {
-            pageIndicator.textContent = "";
-        } else {
-            pageIndicator.textContent = "";
-        }
-    }
-
-    // Initial update of page indicator text based on checkbox state
-    updatePageIndicator();
-
-    checkbox.addEventListener("change", function() {
-        if (this.checked) {
-            window.location.href = "dentonList.html"; // Redirect to index.html if checkbox is checked
-        } else {
-            window.location.href = "dentonInteractive.html"; // Redirect to other_page.html if checkbox is unchecked
-        }
-    });
+// Add event listeners to show popups on mouseover
+EurekaPmarker.on('mouseover', function(e){
+    this.openPopup();
 });
-function toggleNav() {
-    var sidenav = document.getElementById("mySidenav");
-    if (sidenav.style.width == "200px") {
-      closeNav();
-    } else {
-      openNav();
-    }
-  }
-  
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "200px";
-    document.getElementById("main").style.marginRight = "200px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight= "0";
-  }
-  
+NorthLakesPmarker.on('mouseover', function(e){
+    this.openPopup();
+});
+LakeForestPmarker.on('mouseover', function(e){
+    this.openPopup();
+});
+MckennaParkmarker.on('mouseover', function(e){
+    this.openPopup();
+});
+WheelerRidgePmarker.on('mouseover', function(e){
+    this.openPopup();
+});
+
+var popup1 = L.popup({
+    maxWidth: 650, // Adjust maxWidth as needed
+    maxHeight: 100 // Adjust maxHeight as needed
+  }).setContent(park1);
+
+  var popup2 = L.popup({
+    maxWidth: 300, // Adjust maxWidth as needed
+    maxHeight: 200 // Adjust maxHeight as needed
+  }).setContent(park2);
+
+  var popup3 = L.popup({
+    maxWidth: 300, // Adjust maxWidth as needed
+    maxHeight: 200 // Adjust maxHeight as needed
+  }).setContent(park3);
+
+  var popup4 = L.popup({
+    maxWidth: 300, // Adjust maxWidth as needed
+    maxHeight: 200 // Adjust maxHeight as needed
+  }).setContent(park4);
+
+  var popup5 = L.popup({
+    maxWidth: 300, // Adjust maxWidth as needed
+    maxHeight: 200 // Adjust maxHeight as needed
+  }).setContent(park5);
