3a6e3e8464e902b3b105a224be8316192aba14df
Bernice <uchechichiaha@gmail.com>
2025-03-31 22:39:14 -0500

    updating the remote repo

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,7 +5,7 @@ describe("Login Page Tests", () => {
         cy.get("#email").type("bchiaha@gmail.com");
         cy.get("#password").type("Newpassword@2022");
         cy.get("#login-btn").click();
-        cy.url().should("include", "dentonInteractive.html");
+        cy.url().should("include", "dentonList.html");
     });
     it("should show an error for empty fields", () => {
         cy.visit("http://127.0.0.1:5500/parcs-2/loginpage2.html");
@@ -20,8 +20,9 @@ describe("Login Page Tests", () => {
       });
       it("should show an error for empty password", () => {
         cy.visit("http://127.0.0.1:5500/parcs-2/loginpage2.html");
+        cy.get("#email").type("invalidEmail@example.com");
         cy.get("#password").clear();
         cy.get("#login-btn").click();
-        cy.get("#error-message").should("contain", "Password is required");
+        cy.get("#error-message").should("contain", "Password is required.");
     });
 });
