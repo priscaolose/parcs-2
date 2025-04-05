da7338ee6e2e470103f033e56dbfd2f2a4207260
Bernice <uchechichiaha@gmail.com>
2025-04-05 03:53:18 -0500

    trying to merge my branch to main

--- cypress/e2e/about.cy.js
+++ cypress/e2e/about.cy.js
@@ -1,3 +1,18 @@
 describe("About Page Tests", () => {
+    beforeEach(() => {
+        cy.visit('/');
+        });
+    if("should redirect to the login page when user clicks on about",()=>{
+        cy.get("#findParksBtn").click();
+        cy.url().should("include", "loginpage2.html");
+    });
+    it('should load the homepage', () => {
+        cy.get('title').should('contain', 'Parcs');
+        cy.get('.header').should('be.visible');
+      });
 
+    it('should navigate to the login page when Find Parks is clicked', () => {
+    cy.get('#findParksBtn').click();
+    cy.url().should('include', 'loginpage2.html');
+    });
 });
